ff73e58427bf717354882e57db12b58707abccc0
Luke <lukas.zahoransky@zsbystricany.info>
2020-09-19 16:16:58 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamtape-api-python-scaldings", 
+    version="1.0",
+    author="scaldings",
+    author_email="lukedahlia@gmail.com",
+    description=A Streamtape API written in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scaldings/streamtape-api-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
